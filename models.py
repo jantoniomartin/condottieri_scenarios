c572d64c37b92f8d9b34d3916a885144954fac21
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2013-11-20 21:44:45 +0100

    change ordering of scenarios

--- models.py
+++ models.py
@@ -141,7 +141,7 @@ class Scenario(models.Model):
 	class Meta:
 		verbose_name = _("scenario")
 		verbose_name_plural = _("scenarios")
-		ordering = ["start_year",]
+		ordering = ["setting", "start_year",]
 		translate = ('title', 'description',)
 
 	def save(self, *args, **kwargs):
